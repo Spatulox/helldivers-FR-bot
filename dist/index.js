9de41b5b41e6ab078b6ff3df017247a1c4fa6cf8
Spatulox <M78stormtrooper@laposte.net>
2025-04-11 11:12:39 +0200

    add index.js

---
+++ dist/index.js
@@ -0,0 +1,89 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+//Librairies
+const discord_js_1 = require("discord.js");
+// functions
+const log_1 = require("./utils/log");
+const checkInternetCo_1 = require("./utils/server/checkInternetCo");
+const executeCommand_1 = require("./commands/executeCommand");
+const executeModalSubmit_1 = require("./form/executeModalSubmit");
+const executeSelectmenu_1 = require("./selectmenu/executeSelectmenu");
+const login_1 = require("./utils/login");
+const client_1 = require("./utils/client");
+function main() {
+    return __awaiter(this, void 0, void 0, function* () {
+        (0, log_1.log)('INFO : ----------------------------------------------------');
+        (0, log_1.log)('INFO : Starting Program');
+        yield (0, checkInternetCo_1.checkInternetCo)();
+        (0, log_1.log)(`INFO : Using discord.js version: ${discord_js_1.version}`);
+        (0, log_1.log)('INFO : Trying to connect to Discord Servers');
+        if (!(0, login_1.loginBot)(client_1.client)) {
+            (0, log_1.log)('INFO : Stopping program');
+            process.exit();
+        }
+        client_1.client.on('ready', () => __awaiter(this, void 0, void 0, function* () {
+            //loadScheduledJobs()
+            //checkAndUpdateMembers();
+            if (client_1.client && client_1.client.user) {
+                (0, log_1.log)(`INFO : ${client_1.client.user.username} has logged in, waiting...`);
+            }
+            (0, login_1.setActivity)(client_1.client, 'La Démocratie', discord_js_1.ActivityType.Watching);
+        }));
+        client_1.client.on('interactionCreate', (interaction) => __awaiter(this, void 0, void 0, function* () {
+            try {
+                executeSelectmenu_1.executeSelectMenu;
+                if (interaction.isCommand()) {
+                    // Si l'interaction est une commande slash
+                    (0, executeCommand_1.executeSlashCommand)(interaction);
+                }
+                else if (interaction.isModalSubmit()) {
+                    // Si l'interaction est un modal submit
+                    (0, executeModalSubmit_1.executeModalSubmit)(interaction);
+                }
+                else if (interaction.isStringSelectMenu()) {
+                    // Si l'interaction est un selectMenu
+                    (0, executeSelectmenu_1.executeSelectMenu)(interaction);
+                }
+                else {
+                    console.warn(`WARN : Type d'interaction non pris en charge (${interaction.type})`);
+                }
+            }
+            catch (error) {
+                console.error(`ERROR : Une erreur s'est produite lors du traitement de l'interaction`, error);
+            }
+        }));
+        /*client.on('guildMemberUpdate', async (oldMember, newMember) => {
+            if (newMember.guild.id === TARGET_GUILD_ID) {
+    
+                if(DO_NOT_AFFECT_THIS_USERS.includes(newMember.user.id) || newMember.user.bot){
+                    console.log(`Skipping user: ${newMember.user.username} (ID: ${newMember.user.id})`);
+                    return;
+                }
+                
+                await handleMemberUpdate(oldMember, newMember);
+            }
+        });
+    
+        client.on('guildMemberAdd', async (member) => {
+            if (member.guild.id === TARGET_GUILD_ID) {
+    
+                if(DO_NOT_AFFECT_THIS_USERS.includes(member.user.id) || member.user.bot){
+                    console.log(`Skipping user: ${member.user.username} (ID: ${member.user.id})`);
+                    return;
+                }
+    
+                await handleNewMember(member);
+            }
+        });*/
+    });
+}
+main();
