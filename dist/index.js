df44995d4063f83132b58ba6990345559beeb9b0
Spatulox <M78stormtrooper@laposte.net>
2025-04-25 19:37:14 +0200

    remove the counter function call

--- dist/index.js
+++ dist/index.js
@@ -38,7 +38,7 @@ function main() {
         client_1.client.on('ready', () => __awaiter(this, void 0, void 0, function* () {
             //loadScheduledJobs()
             //checkAndUpdateMembers();
-            (0, counter_1.initializeCounter)();
+            //initializeCounter()
             if (client_1.client && client_1.client.user) {
                 (0, log_1.log)(`INFO : ${client_1.client.user.username} has logged in, waiting...`);
             }
