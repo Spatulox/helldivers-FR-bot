99502689b5335f33ac2a32c7eabb6a7683e4f89f
Spatulox <M78stormtrooper@laposte.net>
2025-06-11 18:46:53 +0200

    Adds command deployment functionality
    Adds scripts to deploy and delete Discord bot commands and context menus, both globally and to specific guilds.
    
    These scripts handle reading command definitions from JSON files, registering them with Discord, and logging the deployment status.
    Also implements permission flag handling.

---
+++ dist/builder/delete.js
@@ -0,0 +1,62 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const rest_1 = require("@discordjs/rest");
+const v10_1 = require("discord-api-types/v10");
+const config_json_1 = __importDefault(require("../config.json"));
+const log_1 = require("../utils/log");
+function deleteAllCommandsAndMenus() {
+    return __awaiter(this, void 0, void 0, function* () {
+        const rest = new rest_1.REST({ version: '10' }).setToken(config_json_1.default.token);
+        const guildIDs = ["1111160769132896377", "1214320754578165901"]; // Liste des guildes à vérifier
+        try {
+            // 1. Suppression des commandes/menus globaux
+            (0, log_1.log)('INFO : Suppression des commandes et menus contextuels globaux...');
+            const globalCommands = yield rest.get(v10_1.Routes.applicationCommands(config_json_1.default.clientId));
+            for (const command of globalCommands) {
+                try {
+                    yield rest.delete(v10_1.Routes.applicationCommand(config_json_1.default.clientId, command.id));
+                    (0, log_1.log)(`SUCCÈS : Commande/menu contextuel global "${command.name}" supprimé.`);
+                }
+                catch (err) {
+                    (0, log_1.log)(`ERREUR : Impossible de supprimer la commande/menu contextuel global "${command.name}" : ${err.message}`);
+                }
+            }
+            // 2. Suppression des commandes/menus spécifiques aux guildes
+            (0, log_1.log)('INFO : Suppression des commandes et menus contextuels spécifiques aux guildes...');
+            for (const guildId of guildIDs) {
+                try {
+                    const guildCommands = yield rest.get(v10_1.Routes.applicationGuildCommands(config_json_1.default.clientId, guildId));
+                    for (const command of guildCommands) {
+                        try {
+                            yield rest.delete(v10_1.Routes.applicationGuildCommand(config_json_1.default.clientId, guildId, command.id));
+                            (0, log_1.log)(`SUCCÈS : Commande/menu contextuel "${command.name}" supprimé sur la guilde ${guildId}.`);
+                        }
+                        catch (err) {
+                            (0, log_1.log)(`ERREUR : Impossible de supprimer la commande/menu contextuel "${command.name}" sur la guilde ${guildId} : ${err.message}`);
+                        }
+                    }
+                }
+                catch (err) {
+                    (0, log_1.log)(`ERREUR : Impossible de récupérer ou supprimer les commandes/menus contextuels pour le serveur ${guildId} : ${err.message}`);
+                }
+            }
+            (0, log_1.log)('INFO : Toutes les commandes et menus contextuels ont été supprimés.');
+        }
+        catch (err) {
+            (0, log_1.log)(`ERREUR CRITIQUE : Impossible de récupérer ou supprimer les commandes/menus contextuels : ${err.message}`);
+        }
+    });
+}
+deleteAllCommandsAndMenus();
---
+++ dist/builder/deploy.js
@@ -0,0 +1,128 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __rest = (this && this.__rest) || function (s, e) {
+    var t = {};
+    for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p) && e.indexOf(p) < 0)
+        t[p] = s[p];
+    if (s != null && typeof Object.getOwnPropertySymbols === "function")
+        for (var i = 0, p = Object.getOwnPropertySymbols(s); i < p.length; i++) {
+            if (e.indexOf(p[i]) < 0 && Object.prototype.propertyIsEnumerable.call(s, p[i]))
+                t[p[i]] = s[p[i]];
+        }
+    return t;
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.deployAllCommands = deployAllCommands;
+const client_1 = require("../utils/client");
+const files_1 = require("../utils/server/files");
+const v10_1 = require("discord-api-types/v10");
+const log_1 = require("../utils/log");
+const login_1 = require("../utils/login");
+const rest_1 = require("@discordjs/rest");
+const v10_2 = require("discord-api-types/v10");
+const config_json_1 = __importDefault(require("../config.json"));
+const promises_1 = require("timers/promises");
+const UnitTime_1 = require("../utils/times/UnitTime");
+// Initialisation du REST après la création du client
+client_1.client.rest = new rest_1.REST({ version: '10' }).setToken(config_json_1.default.token);
+function deployAllCommands() {
+    return __awaiter(this, void 0, void 0, function* () {
+        if (!(yield (0, login_1.loginBot)(client_1.client))) {
+            (0, log_1.log)("Erreur : Impossible de connecter le bot");
+            return;
+        }
+        client_1.client.once("ready", () => __awaiter(this, void 0, void 0, function* () {
+            (0, log_1.log)('INFO : Déploiement des commandes slash et menus contextuels');
+            // 1. Lire les commandes slash
+            const slashFiles = yield (0, files_1.listJsonFile)('./commands/');
+            // 2. Lire les menus contextuels
+            const contextFiles = yield (0, files_1.listJsonFile)('./context-menu/');
+            if (!slashFiles || !contextFiles) {
+                (0, log_1.log)('ERREUR : Impossible de lire les fichiers de commandes ou de menus contextuels');
+                return;
+            }
+            // Tableaux pour les commandes globales (sans guildID)
+            const globalCommands = [];
+            let totalDeployed = 0;
+            let totalFiles = 0;
+            // Fonction pour traiter un dossier de commandes
+            function processCommands(folderPath_1) {
+                return __awaiter(this, arguments, void 0, function* (folderPath, isContextMenu = false) {
+                    const files = yield (0, files_1.listJsonFile)(folderPath);
+                    if (!Array.isArray(files))
+                        return;
+                    const filteredFiles = files.filter(f => !f.includes("example"));
+                    totalFiles += filteredFiles.length;
+                    for (const file of filteredFiles) {
+                        try {
+                            const command = yield (0, files_1.readJsonFile)(`${folderPath}${file}`);
+                            if (command.defaultMemberPermissions && Array.isArray(command.defaultMemberPermissions)) {
+                                const bitfield = command.defaultMemberPermissions
+                                    .map(perm => {
+                                    const flag = v10_1.PermissionFlagsBits[perm];
+                                    if (flag === undefined) {
+                                        throw new Error(`Permission inconnue : "${perm}". Vérifiez l'orthographe dans votre JSON (Enumeration Discord : PermissionFlagsBits.X ).`);
+                                    }
+                                    return flag;
+                                })
+                                    .reduce((acc, val) => acc | val, BigInt(0));
+                                command.defaultMemberPermissions = [bitfield.toString()];
+                            }
+                            // Déploiement pour des guildes spécifiques ou globalement
+                            if (command.guildID && command.guildID.length > 0) {
+                                for (const guildId of command.guildID) {
+                                    // Créer une copie de la commande sans le paramètre guildID
+                                    const { guildID } = command, commandWithoutGuildID = __rest(command, ["guildID"]);
+                                    try {
+                                        yield client_1.client.rest.put(v10_2.Routes.applicationGuildCommands(config_json_1.default.clientId, guildId), { body: [commandWithoutGuildID] });
+                                        (0, log_1.log)(`SUCCÈS : ${isContextMenu ? 'Menu contextuel' : 'Commande'} "${command.name}" déployée sur la guilde ${guildId}`);
+                                        totalDeployed++;
+                                    }
+                                    catch (err) {
+                                        (0, log_1.log)(`ERREUR : Impossible de déployer ${isContextMenu ? 'le menu contextuel' : 'la commande'} "${command.name}" sur la guilde ${guildId}. Raison : ${err.message}`);
+                                    }
+                                    yield (0, promises_1.setTimeout)(UnitTime_1.Time.second.SEC_01.toMilliseconds());
+                                }
+                            }
+                            else {
+                                (0, log_1.log)(`AJOUT : ${isContextMenu ? 'Menu contextuel' : 'Commande'} "${command.name}" ajouté à la file d'attente globale`);
+                                globalCommands.push(command);
+                                totalDeployed++;
+                            }
+                        }
+                        catch (err) {
+                            (0, log_1.log)(`ERREUR : Lecture du fichier ${file} : ${err.message}`);
+                        }
+                    }
+                });
+            }
+            // Traiter les commandes slash
+            yield processCommands('./commands/');
+            // Traiter les menus contextuels
+            yield processCommands('./context-menu/', true);
+            // Déploiement global des commandes sans guildID
+            if (globalCommands.length > 0) {
+                try {
+                    yield client_1.client.rest.put(v10_2.Routes.applicationCommands(client_1.client.user.id), { body: globalCommands });
+                    (0, log_1.log)(`SUCCÈS : ${globalCommands.length}/${totalFiles} commandes/menus globaux déployés`);
+                }
+                catch (err) {
+                    (0, log_1.log)(`ERREUR CRITIQUE : Déploiement des commandes/menus globaux : ${err.message}`);
+                }
+            }
+            process.exit();
+        }));
+    });
+}
+deployAllCommands();
