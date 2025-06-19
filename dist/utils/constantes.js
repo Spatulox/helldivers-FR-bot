0f6bd81c7e220effe33b5d012f83fba1b7fec025
Spatulox <M78stormtrooper@laposte.net>
2025-06-19 09:45:27 +0200

    Update constantes.js: Adds discord regex
    Adds regex for discord mentions, including users, roles and channels.
    Also includes regex for folders, files and URLS

--- dist/utils/constantes.js
+++ dist/utils/constantes.js
@@ -3,7 +3,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.URL_REGEX = exports.WIKI_FILE_REGEX = exports.WIKI_FOLDER_REGEX = exports.DO_NOT_AFFECT_THIS_USERS = exports.MAX_NICKNAME_LENGTH = exports.TARGET_GUILD_ID = exports.regexSEIC = exports.regexRole = exports.STAR_EMOJI = exports.PRIORITY_EMOJI = exports.CHECK_HOURS = void 0;
+exports.DISCORD_MENTION_REGEX = exports.DISCORD_PING_REGEX = exports.ROLE_REGEX = exports.CHANNEL_REGEX = exports.USER_REGEX = exports.URL_REGEX = exports.WIKI_FILE_REGEX = exports.WIKI_FOLDER_REGEX = exports.DO_NOT_AFFECT_THIS_USERS = exports.MAX_NICKNAME_LENGTH = exports.TARGET_GUILD_ID = exports.regexSEIC = exports.regexRole = exports.STAR_EMOJI = exports.PRIORITY_EMOJI = exports.CHECK_HOURS = void 0;
 const config_json_1 = __importDefault(require("../config.json"));
 exports.CHECK_HOURS = 2;
 exports.PRIORITY_EMOJI = ["🦆"];
@@ -13,7 +13,22 @@ exports.regexSEIC = new RegExp(`\\[SEIC\\]`);
 //export const regex = /\[(\d+(?:\+|☆)?|PRIORITY_EMOJI|\?+)\]/; // take : [\d\+] and [\d☆] and [🦆] and [?]
 exports.TARGET_GUILD_ID = config_json_1.default.guildId;
 exports.MAX_NICKNAME_LENGTH = 32;
-exports.DO_NOT_AFFECT_THIS_USERS = ["877326929869561877"];
+exports.DO_NOT_AFFECT_THIS_USERS = ["877326929869561877"]; // Gounie
 exports.WIKI_FOLDER_REGEX = /<:([a-zA-Z0-9_]+):(\d+)>/;
 exports.WIKI_FILE_REGEX = /\(([a-zA-Z0-9_]+)-(\d+)\)/;
 exports.URL_REGEX = /(https?:\/\/[^s]+)/;
+/* DISCORD REGEX */
+exports.USER_REGEX = /<@\d{19}>/;
+exports.CHANNEL_REGEX = /<#\d{19}>|<id:(browse|customize|guide)>/;
+exports.ROLE_REGEX = /<@&\d{19}>/;
+/**
+ * Mention a User
+ * Mention a Role
+ */
+exports.DISCORD_PING_REGEX = new RegExp(`${exports.USER_REGEX.source}|${exports.ROLE_REGEX.source}`, 'g');
+/**
+ * Mention a User
+ * Mention a Role
+ * Mention a Channnel
+ */
+exports.DISCORD_MENTION_REGEX = new RegExp(`${exports.DISCORD_PING_REGEX.source}|${exports.CHANNEL_REGEX.source}`, 'g');
