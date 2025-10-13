093d62d15e69910917e028e76c106e5853ef5f2f
Spatulox <M78stormtrooper@laposte.net>
2025-10-13 15:34:20 +0200

    All those file has been reworked to their dedicated folder/class
    When synching files, it don't delete the deleted files in the main repo

--- dist/constantes.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.WIKI_FILE_REGEX = exports.WIKI_FOLDER_REGEX = void 0;
-exports.WIKI_FOLDER_REGEX = /<:([a-zA-Z0-9_]+):(\d+)>/;
-exports.WIKI_FILE_REGEX = /\(([a-zA-Z0-9_]+)-(\d+)\)/;
