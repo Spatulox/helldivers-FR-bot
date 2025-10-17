3e736817621dd90ee8506a45a8d05f64d14bfb99
Spatulox <M78stormtrooper@laposte.net>
2025-10-17 09:32:28 +0200

    Update Statistics: Comment average message
    Comment the average message functionality from the
    statistics module.

--- dist/modules/Functionnalities/statistiques/Statistics.js
+++ dist/modules/Functionnalities/statistiques/Statistics.js
@@ -3,20 +3,20 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.Statistics = void 0;
 const Modules_1 = require("../../../utils/other/Modules");
 const ActiveMembers_1 = require("./ActiveMembers");
-const AverageMessage_1 = require("./AverageMessage");
+//import { AverageMessage } from "./AverageMessage";
 class Statistics extends Modules_1.MultiModule {
+    //public readonly averageMessage = new AverageMessage()
     constructor() {
         super("Statistics", "Module to handle different Statistics");
         this.activeMember = new ActiveMembers_1.ActiveMember();
-        this.averageMessage = new AverageMessage_1.AverageMessage();
         this._subModuleList = [
             this.activeMember,
-            this.averageMessage
+            //this.averageMessage
         ];
     }
     handleMessage(message) {
         this.activeMember.handleMessage(message);
-        this.averageMessage.handleMessage(message);
+        //this.averageMessage.handleMessage(message)
     }
 }
 exports.Statistics = Statistics;
