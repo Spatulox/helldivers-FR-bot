850790dd54342355072bd503156ae8c64bb0d680
Spatulox <M78stormtrooper@laposte.net>
2025-10-26 14:25:08 +0100

    Adds a new priority emoji
    Extends the list of priority emojis to include a new symbol,
    enhancing the flexibility of priority tagging.

--- dist/utils/constantes.js
+++ dist/utils/constantes.js
@@ -6,7 +6,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.DISCORD_MENTION_REGEX = exports.DISCORD_PING_REGEX = exports.ROLE_REGEX = exports.CHANNEL_REGEX = exports.BOT_REGEX = exports.USER_REGEX = exports.URL_REGEX = exports.WIKI_FILE_REGEX = exports.WIKI_FOLDER_REGEX = exports.DO_NOT_AFFECT_THIS_USERS = exports.MAX_NICKNAME_LENGTH = exports.AMIRAL_SUPER_TERRE_ID = exports.TARGET_GUILD_ID = exports.SPACE = exports.regexSEIC = exports.regexRole = exports.STAR_EMOJI = exports.PRIORITY_EMOJI = exports.CHECK_HOURS = void 0;
 const config_json_1 = __importDefault(require("../config.json"));
 exports.CHECK_HOURS = 2;
-exports.PRIORITY_EMOJI = ["🦆"];
+exports.PRIORITY_EMOJI = ["🦆", "✭"];
 exports.STAR_EMOJI = "☆";
 exports.regexRole = new RegExp(`\\[(\\d+(?:\\+|${exports.STAR_EMOJI})?|${exports.PRIORITY_EMOJI.join("|")}|\\?+|\d-\d)\\]`); // take : [\d\+] and [\d☆] and [🦆] and [?] and [1-9]
 exports.regexSEIC = new RegExp(`\\[SEIC\\]`);
