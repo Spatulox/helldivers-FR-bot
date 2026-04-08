c125d92bfcabf468c98a628915f3685584441de4
Spatulox <M78stormtrooper@laposte.net>
2026-04-08 14:38:30 +0200

    add images to img root folder

--- dist/utils/HDFRIntegrationList.js
+++ dist/utils/HDFRIntegrationList.js
@@ -3,11 +3,11 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.HDFRIntegrationList = void 0;
 const _M4R4UD3R = {
     name: "M4R4UD3R",
-    avatarUrl: "./src/img/marauder.png",
+    avatarUrl: "./img/marauder.png",
 };
 const _AMIRAL_SUPER_TERRE = {
     name: "Amiral Super Terre",
-    avatarUrl: "./src/img/amiral_super_terre.jpg",
+    avatarUrl: "./img/amiral_super_terre.jpg",
 };
 exports.HDFRIntegrationList = {
     M4R4UD3R: _M4R4UD3R,
