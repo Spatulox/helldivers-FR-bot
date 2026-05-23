961e6ba1c11c7742beb2d9eb2cd22ad6193f7d1b
Spatulox <M78stormtrooper@laposte.net>
2026-05-23 23:36:49 +0200

    Enable manual HDFR member update via Discord command
    Allow HDFR technicians to trigger the member update process on demand by typing `check all -force` in the designated `#bot_et_brouillons` channel. This provides flexibility for immediate updates outside the regular scheduled cron job.

--- src/hdfr/src/modules/hdfr_private_functionnalities/ScheduleJobs/UpdateAllMembers.js
+++ src/hdfr/src/modules/hdfr_private_functionnalities/ScheduleJobs/UpdateAllMembers.js
@@ -12,7 +12,16 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.UpdateAllMembers = void 0;
 const ExecuteTaskWithCron_1 = require("../../../../../share/modules/ExecuteTaskWithCron");
 const HDFRMember_1 = require("../HDFRMember");
+const discord_js_1 = require("discord.js");
+const HDFR_1 = require("../../../utils/hdfr_list/HDFR");
+const GlobalMemberManager_1 = require("../../../../../share/managers/GlobalMemberManager");
+const simplediscordbot_1 = require("@spatulox/simplediscordbot");
 class UpdateAllMembers extends ExecuteTaskWithCron_1.ExecuteTaskWithCron {
+    get events() {
+        return {
+            [discord_js_1.Events.MessageCreate]: this.handleMessage.bind(this),
+        };
+    }
     constructor() {
         //super('0 */2 * * *', async () => await HDFRMember.checkAndUpdateMembers());
         super('0 */2 * * *', () => __awaiter(this, void 0, void 0, function* () {
@@ -21,6 +30,24 @@ class UpdateAllMembers extends ExecuteTaskWithCron_1.ExecuteTaskWithCron {
         this.name = "UpdateAllMembers";
         this.description = "Automatically update all members every 2 hours";
         this.members = new HDFRMember_1.NewHDFRMember();
+    }
+    handleMessage(message) {
+        return __awaiter(this, void 0, void 0, function* () {
+            try {
+                if (message.channelId == HDFR_1.HDFR.channel.bot_brouillons &&
+                    message.member &&
+                    GlobalMemberManager_1.GlobalMemberManager.HDFR.isTechnician(message.member) &&
+                    message.content == "check all -force") {
+                    simplediscordbot_1.Bot.log.info(simplediscordbot_1.EmbedManager.debug(`Update All members force started by <@${message.member.id}>`));
+                    message.deletable && (yield message.delete());
+                    yield this.members.checkAndUpdateMembers();
+                    simplediscordbot_1.Bot.log.info(simplediscordbot_1.EmbedManager.debug(`Update All members finished`));
+                }
+            }
+            catch (e) {
+                console.log(e);
+            }
+        });
     }
 }
 exports.UpdateAllMembers = UpdateAllMembers;
