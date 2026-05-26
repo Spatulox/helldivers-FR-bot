66507a7f61a19571c1309377605896727efcbbec
Spatulox <M78stormtrooper@laposte.net>
2026-05-26 13:22:07 +0200

    add MiscStats module

--- src/hdfr/src/modules/hdfr_private_functionnalities/AutoBanScamHDFR.js
+++ src/hdfr/src/modules/hdfr_private_functionnalities/AutoBanScamHDFR.js
@@ -1,10 +1,35 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.AutoBanScamHDFR = void 0;
 const AutoBanScam_1 = require("../../../../share/modules/AutoBanScam");
 const HDFR_1 = require("../../utils/hdfr_list/HDFR");
 const GlobalMemberManager_1 = require("../../../../share/managers/GlobalMemberManager");
+const MiscStatistics_1 = require("../statistiques/MiscStatistics");
 class AutoBanScamHDFR extends AutoBanScam_1.AutoBanScam {
+    constructor() {
+        super();
+    }
+    neRienEcrireIci(message) {
+        const _super = Object.create(null, {
+            neRienEcrireIci: { get: () => super.neRienEcrireIci }
+        });
+        return __awaiter(this, void 0, void 0, function* () {
+            if (message.channelId != this.neRienEcrireIciChannel) {
+                return;
+            }
+            yield MiscStatistics_1.MiscStatistics.incrementAutoBanScam();
+            yield _super.neRienEcrireIci.call(this, message);
+        });
+    }
     get guildId() {
         return HDFR_1.HDFR.guildID;
     }
--- src/hdfr/src/modules/statistiques/Statistics.js
+++ src/hdfr/src/modules/statistiques/Statistics.js
@@ -3,13 +3,15 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.Statistics = void 0;
 const ActiveMembers_1 = require("./ActiveMembers");
 const discord_module_1 = require("@spatulox/discord-module");
+const MiscStatistics_1 = require("./MiscStatistics");
 class Statistics extends discord_module_1.MultiModule {
     constructor() {
         super(...arguments);
         this.name = "Statistics";
         this.description = "Module to handle different Statistics";
         this.subModules = [
-            new ActiveMembers_1.ActiveMember()
+            new ActiveMembers_1.ActiveMember(),
+            new MiscStatistics_1.MiscStatistics(),
         ];
     }
 }
